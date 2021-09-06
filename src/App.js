2de73cdc791d26aa1aa91b0b2a7d1b275a05c91e
Andrew Burnie <andrew.burnie@mattereum.com>
2021-09-06 19:24:43 +0100

    adds image upload/delete functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const [imageSRC, setImageSRC] = useState(""); 
+    const onImageChange = event => {
+        if (event.target.files && event.target.files[0]){
+            const img = event.target.files[0];
+            setImageSRC(URL.createObjectURL(img));
+            }
+        }
+
+    const deleteImage = () => {setImageSRC("")}
+
+    let displayArea;
+
+    if (imageSRC) {
+        displayArea = <img src={imageSRC} alt="uploaded"/>;
+        }
+
+    if (!imageSRC) {
+        displayArea = <div>No Image</div>
+        }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+        Welcome to Image Viewer
+        </h1>
+              </header>
+              <label htmlFor="imageUpload">Upload Image</label>
+              <input id="imageUpload" type="file" onChange={onImageChange}/>
+              <button id="imageDelete" onClick={deleteImage}>Delete Image</button>
+              {displayArea}
+              
+
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('start with Welcome Page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeElement = screen.getByText(/Welcome to Image Viewer/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
+
+test('Image upload button exists', () => {
+    render(<App />);
+    const imageButton = screen.getByLabelText(/Upload Image/);
+    expect(imageButton).toBeInTheDocument();
+    })
+
+test('At start see no image', () => {
+    render(<App />);
+    const emptyImageDiv = screen.getByText(/No Image/);
+    expect(emptyImageDiv).toBeInTheDocument();
+    })
+
+test('Can upload image', () => {
+    render(<App />);
+    global.URL.createObjectURL = jest.fn(() => 'test.jpg');
+    const file = new File(['hello'], '../exampleImages/20210906_173220.jpg', {type: 'image/jpg'})
+    const imageButton = screen.getByLabelText(/Upload Image/);
+    userEvent.upload(imageButton, file);
+    expect(imageButton.files[0]).toStrictEqual(file);
+
+    const uploadedImage = screen.getByAltText(/uploaded/);
+    expect(uploadedImage).toHaveAttribute('src', 'test.jpg');
+    })
+
+test('Can remove image', () => {
+    render(<App />);
+    global.URL.createObjectURL = jest.fn(() => 'test.jpg');
+    const file = new File(['hello'], '../exampleImages/20210906_173220.jpg', {type: 'image/jpg'})
+    const imageButton = screen.getByLabelText(/Upload Image/);
+    userEvent.upload(imageButton, file);
+
+    userEvent.click(screen.getByText('Delete Image'));
+    const emptyImageDiv = screen.getByText(/No Image/);
+    expect(emptyImageDiv).toBeInTheDocument();   
+
+    })
