6d52f20b38e0ad98a842561a6b595a9cc925a8c1
Andrew Burnie <apb@debian.andrew>
2021-09-09 14:45:04 +0100

    can resize images to zoom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-    const [imageSRC, setImageSRC] = useState(""); 
+    const [imageSRC, setImageSRC] = useState("");
+    const [scale, setScale] = useState(1);
+    
     const onImageChange = event => {
         if (event.target.files && event.target.files[0]){
             const img = event.target.files[0];
@@ -14,8 +16,35 @@ function App() {
 
     let displayArea;
 
+    useEffect(()=> {
+		function keyHandle(event){
+		console.log(event);
+		if (event.key === "z"){
+			setScale(scale => 1.1*scale);
+			}
+		if (event.key === "x"){
+			setScale(scale => scale/1.1)
+			}
+		}
+
+		document.addEventListener('keydown', keyHandle);
+
+		 return function cleanup() {
+      document.removeEventListener('keydown', keyHandle);
+    }
+		
+		}, []);
+	 
+
+    
+
     if (imageSRC) {
-        displayArea = <img src={imageSRC} alt="uploaded"/>;
+		
+        displayArea = (
+        <div id="imageBox">
+        <img src={imageSRC} alt="uploaded" id="uploadedImage" style={{transform: "scale(" + scale + ")"  }}/>
+        </div>
+        );
         }
 
     if (!imageSRC) {
@@ -30,13 +59,12 @@ function App() {
         Welcome to Image Viewer
         </h1>
               </header>
+              <div>
               <label htmlFor="imageUpload">Upload Image</label>
               <input id="imageUpload" type="file" onChange={onImageChange}/>
               <button id="imageDelete" onClick={deleteImage}>Delete Image</button>
+              </div>
               {displayArea}
-              
-
-
     </div>
   );
 }
