3cb87b2746c28524108a0556f7db450ff255d863
Andrew Burnie <apburnie@hotmail.co.uk>
2021-12-14 15:46:07 +0000

    adds prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,115 +1,113 @@
-import './App.css';
-import React from 'react';
-import html2canvas from 'html2canvas';
-import { useState, useEffect } from 'react';
-
+import "./App.css";
+import React from "react";
+import html2canvas from "html2canvas";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [imageSRC, setImageSRC] = useState("");
-    const [scale, setScale] = useState(40);
-    const [x, setX] = useState(50);
-    const [y, setY] = useState(50);
-    
-    const onImageChange = event => {
-        if (event.target.files && event.target.files[0]){
-            const img = event.target.files[0];
-            setImageSRC(URL.createObjectURL(img));
-            }
-        }
+  const [imageSRC, setImageSRC] = useState("");
+  const [scale, setScale] = useState(40);
+  const [x, setX] = useState(50);
+  const [y, setY] = useState(50);
 
-    const deleteImage = () => {setImageSRC("")}
-
-    const exportImage = () => html2canvas(document.querySelector("#uploadedImage")).then(canvas => {
-        let div = document.querySelector("#snapshot")
-        if (div.firstChild) {
-            div.removeChild(div.firstChild)
-        }
-        div.append(canvas);
-        })
-
-    let displayArea;
-
-    useEffect(()=> {
-		function keyHandle(event){
-
-			switch(event.key) {
-				case "z":
-				setScale(scale => 1.1*scale);
-				break;
-				case "x":
-				setScale(scale => scale/1.1);
-				break;
-				case "ArrowLeft":
-                                case "h":
-				setX(x => x-5)
-				break;
-				case "ArrowRight":
-                                case "l":
-				setX(x => x+5)
-				break;
-				case "ArrowUp":
-                                case "j":
-				setY(y => y-5)
-				break;
-				case "ArrowDown":
-                                case "k":
-				setY(y => y+5)
-				break;
-				default:
-				break;
-				
-				}
-		
-		}
-
-		document.addEventListener('keydown', keyHandle);
-
-		return function cleanup() {
-      document.removeEventListener('keydown', keyHandle);
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const img = event.target.files[0];
+      setImageSRC(URL.createObjectURL(img));
     }
-		
-		}, []);    
+  };
 
-    if (imageSRC) {
-		
-        displayArea = (
-        <div id="imageBox">
-        <div id="uploadedImage" 
+  const deleteImage = () => {
+    setImageSRC("");
+  };
+
+  const exportImage = () =>
+    html2canvas(document.querySelector("#uploadedImage")).then((canvas) => {
+      let div = document.querySelector("#snapshot");
+      if (div.firstChild) {
+        div.removeChild(div.firstChild);
+      }
+      div.append(canvas);
+    });
+
+  let displayArea;
+
+  useEffect(() => {
+    function keyHandle(event) {
+      switch (event.key) {
+        case "z":
+          setScale((scale) => 1.1 * scale);
+          break;
+        case "x":
+          setScale((scale) => scale / 1.1);
+          break;
+        case "ArrowLeft":
+        case "h":
+          setX((x) => x - 5);
+          break;
+        case "ArrowRight":
+        case "l":
+          setX((x) => x + 5);
+          break;
+        case "ArrowUp":
+        case "j":
+          setY((y) => y - 5);
+          break;
+        case "ArrowDown":
+        case "k":
+          setY((y) => y + 5);
+          break;
+        default:
+          break;
+      }
+    }
+
+    document.addEventListener("keydown", keyHandle);
+
+    return function cleanup() {
+      document.removeEventListener("keydown", keyHandle);
+    };
+  }, []);
+
+  if (imageSRC) {
+    displayArea = (
+      <div id="imageBox">
+        <div
+          id="uploadedImage"
           style={{
-              backgroundImage: "url('"+ imageSRC + "')", 
-              backgroundPosition: `${x}% ${y}%`, 
-              backgroundSize: `${scale}%`, 
-              backgroundRepeat: "no-repeat", 
-              }}/>
-        </div>
-        );
-        }
+            backgroundImage: "url('" + imageSRC + "')",
+            backgroundPosition: `${x}% ${y}%`,
+            backgroundSize: `${scale}%`,
+            backgroundRepeat: "no-repeat",
+          }}
+        />
+      </div>
+    );
+  }
 
-    if (!imageSRC) {
-        displayArea = <div>No Image</div>
-        }
-
+  if (!imageSRC) {
+    displayArea = <div>No Image</div>;
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-        Welcome to Image Viewer
-        </h1>
+        <h1>Welcome to Image Viewer</h1>
       </header>
 
       <div>
         <label htmlFor="imageUpload">Upload Image</label>
-        <input id="imageUpload" type="file" onChange={onImageChange}/>
-        <button id="imageDelete" onClick={deleteImage}>Delete Loaded Image</button>
-        <button id="imageExport" onClick={exportImage}>Snapshot Image</button>
+        <input id="imageUpload" type="file" onChange={onImageChange} />
+        <button id="imageDelete" onClick={deleteImage}>
+          Delete Loaded Image
+        </button>
+        <button id="imageExport" onClick={exportImage}>
+          Snapshot Image
+        </button>
       </div>
 
       <div>
-        <div style={{display: "inline-block"}}>
-          {displayArea}
-        </div>
-        <div id="snapshot" style={{display: "inline-block"}} />
+        <div style={{ display: "inline-block" }}>{displayArea}</div>
+        <div id="snapshot" style={{ display: "inline-block" }} />
         <div>
           <p>Press the following keys to manipulate the image above:</p>
           <table>
--- src/App.test.js
+++ src/App.test.js
@@ -1,54 +1,51 @@
-import React from 'react';
-import { render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
 beforeEach(() => {
-    render(<App />);
-    });
-
-describe('On Home Page', () => {
-
-test('see welcome', () => {
-  const welcomeElement = screen.getByText(/Welcome to Image Viewer/i);
-  expect(welcomeElement).toBeInTheDocument();
+  render(<App />);
 });
 
-test('see button to upload images', () => {
+describe("On Home Page", () => {
+  test("see welcome", () => {
+    const welcomeElement = screen.getByText(/Welcome to Image Viewer/i);
+    expect(welcomeElement).toBeInTheDocument();
+  });
+
+  test("see button to upload images", () => {
     const imageButton = screen.getByLabelText(/Upload Image/);
     expect(imageButton).toBeInTheDocument();
-    })
+  });
 
-test('see div saying no image', () => {
+  test("see div saying no image", () => {
     const emptyImageDiv = screen.getByText(/No Image/);
     expect(emptyImageDiv).toBeInTheDocument();
-    })
-
+  });
 });
 
-describe('Adding/Deleting Images', () => {
-    beforeEach(() => {
-    global.URL.createObjectURL = jest.fn(() => 'test.jpg');
-    });
+describe("Adding/Deleting Images", () => {
+  beforeEach(() => {
+    global.URL.createObjectURL = jest.fn(() => "test.jpg");
+  });
 
-test('can upload image', () => {
-    const file = new File(['hello'], 'test.png', {type: 'image/png'});
+  test("can upload image", () => {
+    const file = new File(["hello"], "test.png", { type: "image/png" });
     const imageButton = screen.getByLabelText(/Upload Image/);
     userEvent.upload(imageButton, file);
     expect(imageButton.files[0]).toStrictEqual(file);
 
     const uploadedImage = screen.getByAltText(/uploaded/);
-    expect(uploadedImage).toHaveAttribute('src', 'test.jpg');
-    })
+    expect(uploadedImage).toHaveAttribute("src", "test.jpg");
+  });
 
-test('can remove image', () => {
-    const file = new File(['hello'], 'test.png', {type: 'image/png'});
+  test("can remove image", () => {
+    const file = new File(["hello"], "test.png", { type: "image/png" });
     const imageButton = screen.getByLabelText(/Upload Image/);
     userEvent.upload(imageButton, file);
 
-    userEvent.click(screen.getByText('Delete Image'));
+    userEvent.click(screen.getByText("Delete Image"));
     const emptyImageDiv = screen.getByText(/No Image/);
-    expect(emptyImageDiv).toBeInTheDocument();   
-
-    })
+    expect(emptyImageDiv).toBeInTheDocument();
+  });
 });
